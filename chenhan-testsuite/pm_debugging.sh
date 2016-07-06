823fa368a39f7947a5305e7d010368e7f89ab2c7
Chen-Han Hsiao (Stanley) <stanley.hsiao@canonical.com>
2016-07-06 15:14:51 +0800

    update

--- chenhan-testsuite/pm_debugging.sh
+++ chenhan-testsuite/pm_debugging.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Suspend the system without give control to bios
+# Ref doc: https://www.kernel.org/doc/Documentation/power/basic-pm-debugging.txt
+#
+# core
+# - test the freezing of processes, suspending of devices, platform global
+#   control methods(*), the disabling of nonboot CPUs and suspending of
+#   platform/system devices
+#
+# Ref doc: https://01.org/blogs/rzhang/2015/best-practice-debug-linux-suspend/hibernate-issues
+
 for i in {1..30}
 do
     echo "starting $i th suspend"
