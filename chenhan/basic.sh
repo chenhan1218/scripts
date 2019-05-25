2c57966e6df030747250e46a34b215b3e54cc459
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2019-05-25 15:34:25 +0800

    add basic

---
+++ chenhan/basic.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install -y zsh git tmux command-not-found
+sudo apt-get update
+sudo update-command-not-found
---
+++ chenhan/kubeadm.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo bash -c 'cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF'
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
